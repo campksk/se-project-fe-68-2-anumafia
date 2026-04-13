cde80c640f6ef2ce3ea0a26827efb005ebd226fb
Khetsopon11 <khetsoponkrongyut@gmail.com>
2026-04-13 22:17:18 +0700

    change-backend-url-in-getUserPf

--- src/libs/getUserProfile.tsx
+++ src/libs/getUserProfile.tsx
@@ -1,5 +1,6 @@
 export default async function getUserProfile(token: string) {
-  const response = await fetch("https://be-ihaveksk.vercel.app/api/v1/auth/me", {
+  const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+  const response = await fetch(`${backendUrl}/api/v1/auth/me`, {
     method: "GET",
     headers: {
       authorization: `Bearer ${token}`,
