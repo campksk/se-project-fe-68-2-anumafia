4b3c05e3268124b1480a1295f433e0c4313dd2b4
Akibara <laanemmone@gmail.com>
2026-04-15 18:06:59 +0700

    Add updateUserProfile function for updating user profile information

---
+++ src/libs/updateUserProfile.ts
@@ -0,0 +1,28 @@
+type UpdateProfile = {
+	name?: string;
+	email?: string;
+	tel?: string;
+};
+
+export default async function updateUserProfile(updateProfile : UpdateProfile, token: string) {
+    const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+    
+	if(!updateProfile.name && !updateProfile.email && !updateProfile.tel) {
+		throw new Error("At least one field must be provided to update the profile");
+	}
+
+    const response = await fetch(`${backendUrl}/api/v1/auth/me`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+        },
+        body: JSON.stringify(updateProfile),
+    });
+
+    if (!response.ok) {
+        throw new Error("Failed to update review");
+    }
+
+    return await response.json();
+}
