7d0be5c1a46e832ec08d39ca6c5b836d1e203c58
Akibara <laanemmone@gmail.com>
2026-04-18 12:59:27 +0700

    Add deleteReview function to handle review deletion

---
+++ src/libs/deleteReview.ts
@@ -0,0 +1,14 @@
+export default async function deleteReview(id: string, token: string) {
+	const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+	const res = await fetch(`${backendUrl}/api/v1/reviews/${id}`, {
+		method: "DELETE",
+		headers: { Authorization: `Bearer ${token}` }
+	});
+
+	if (!res.ok) {
+		throw new Error("Failed to delete review");
+	}
+
+	return await res.json();
+}
