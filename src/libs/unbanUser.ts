9047633834486b1d81564abd7aabf8871c54aec7
polrapat <polrapat@gmail.com>
2026-04-18 18:21:20 +0700

    feat: add yellow card, ban, and unban functionality

---
+++ src/libs/unbanUser.ts
@@ -0,0 +1,12 @@
+export default async function unbanUser(userId: string, token: string) {
+  const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+  const res = await fetch(`${backendUrl}/api/v1/auth/unban/${userId}`, {
+    method: "PUT",
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  return await res.json();
+}
