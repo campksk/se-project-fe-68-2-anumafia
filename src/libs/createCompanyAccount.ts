3d0ee63d6a26b12f71662962296299b8f87feb15
AJ <papanggorn0006@gmail.com>
2026-04-21 20:24:25 +0700

    create API for fetch createCompanyAccout

---
+++ src/libs/createCompanyAccount.ts
@@ -0,0 +1,17 @@
+export default async function createCompanyAccount(token: string, data: any) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/companies`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ ...data, role: "company" }),
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.message || "Failed to create company account");
+  }
+
+  return await response.json();
+}
